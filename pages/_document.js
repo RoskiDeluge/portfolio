5cc73d439019d180991453870ce3a4b15374b16a
Roberto Delgado <roberto.delgado@gmail.com>
2021-04-28 11:01:03 -1000

    Edits to _document data

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,9 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+      title: 'Roberto L. Delgado | Web Developer',
+      description: 'I’m a web developer based in Honolulu, HI.',
+      image: '/images/the_rose_1.jpg'
     }
 
     return (
@@ -24,7 +23,7 @@ class MyDocument extends Document {
           <meta property="og:title" content={meta.title} />
           <meta property="og:image" content={meta.image} />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
+          <meta name="twitter:site" content="@roskideluge" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
           <meta name="twitter:image" content={meta.image} />
