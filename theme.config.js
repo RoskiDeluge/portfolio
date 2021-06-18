3074bf4abb5ab9aed1c004a5a461d210de2961c7
RoskiDeluge <roberto.delgado@gmail.com>
2021-06-18 10:43:36 -1000

    Added social media icons to footer

--- theme.config.js
+++ theme.config.js
@@ -1,8 +1,37 @@
+import React from 'react'
+import Link from 'next/link'
+import {
+  Github,
+  Twitter,
+  Instagram,
+  Youtube
+} from '@icons-pack/react-simple-icons'
 const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
+    <small style={{ display: 'block', marginTop: '4rem' }}>
+      <span
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginBottom: '2rem'
+        }}
+      >
+        <Link href="https://github.com/roskideluge">
+          <Github title="RD Github" color="#808080" size={24} />
+        </Link>
+        <span style={{ paddingLeft: '1rem' }}>
+          <Link href="https://github.com/roskideluge">
+            <Twitter title="RD Twitter" color="#808080" size={24} />
+          </Link>
+        </span>
+        <span style={{ paddingLeft: '1rem' }}>
+          <Link href="https://instagram.com/sfirstm">
+            <Instagram title="RD Instagram" color="#808080" size={24} />
+          </Link>
+        </span>
+      </span>
       <time>{YEAR}</time> © Roberto L. Delgado.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
