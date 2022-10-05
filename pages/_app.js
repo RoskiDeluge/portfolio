1a51696b2675e8f9cf3136933c18a1e2dfc2f494
RoskiDeluge <roberto.delgado@gmail.com>
2022-10-05 10:19:04 -1000

    Refactored GTM again

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-import TagManager from 'react-gtm-module'
+import Script from 'next/script'
 
 import '../styles/main.css'
 
@@ -39,6 +39,15 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PH7X4H4');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   )
