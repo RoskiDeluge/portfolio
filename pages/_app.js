ff796a1e602010bd061bdb8909a65aabca7c3e1e
RoskiDeluge <roberto.delgado@gmail.com>
2022-10-05 10:02:02 -1000

    Refactored GTM script

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-import Script from 'next/script'
+import TagManager from 'react-gtm-module'
 
 import '../styles/main.css'
 
+const tagManagerArgs = {
+  id: "GTM-PH7X4H4",
+}
+
 export default function Nextra({ Component, pageProps }) {
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return (
     <>
       <Head>
@@ -29,18 +38,6 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PH7X4H4');
-        `,
-        }}/>
       <Component {...pageProps} />
     </>
   )
