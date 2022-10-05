ae2a860733a786509364e469885552ed5dad0eca
RoskiDeluge <roberto.delgado@gmail.com>
2022-10-05 08:34:50 -1000

    Added GTM script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import '../styles/main.css'
 
@@ -28,6 +29,18 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PH7X4H4');
+        `,
+        }}/>
       <Component {...pageProps} />
     </>
   )
