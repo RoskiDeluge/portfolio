19ce28f47e21ddde5120b6f460439d213fc1c5a5
RoskiDeluge <roberto.delgado@gmail.com>
2022-12-30 10:23:20 -1000

    Removed gtm script

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-import Script from 'next/script'
 
 import '../styles/main.css'
 
@@ -30,15 +29,6 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PH7X4H4');
-        `}
-      </Script>
       <Component {...pageProps} />
     </>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -31,11 +31,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PH7X4H4" height="0" width="0" style="display:none;visibility:hidden"/>`,
-            }}
-          />
         </body>
       </Html>
     )
